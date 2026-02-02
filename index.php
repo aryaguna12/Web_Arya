c1f6d50cee177d80364cf557bba38e1134e24959
aryaguna12 <aryagunasyarif@gmail.com>
2026-02-02 08:03:01 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,107 +1,65 @@
-<!DOCTYPE html>
+
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kalkulator Sederhana | UKK RPL 2025</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
-    <style type="text/css">
-        .btn {
-            width: 100%;
-        }
-    </style>
-</head>
-<body>
- <div class="container mt-5">
-   <h2 class="text-center">kalkulator Sederhana</h2>
-   <div class="row justify-content-center">
-    <div class="col-md-4">
-        <form method="POST" class="p-2 border rounded bg-light">
-            <label class="form-label">Angka Pertama</label>
-            <input type="number" name="angka1" class="form-control" autocomplete="off" min="0" autofocus
-            required onkeypress="return event.charCode >= 48 && event.charCode <= 57" 
-            value="<?php echo isset($_POST['hasil']) ? $_POST['hasil'] : '' ?>">
-            <label class="form-label">Angka Kedua</label>
-            <input type="number" name="angka2" class="form-control" required>
-             <div class="d-flex justify-content-center gap-2 mt-2">
-                <button type="submit" class="btn btn-primary" name="operator"
-                value="+" title="nambah"><i class="fas fa-plus"></i></button>
-                <button type="submit" class="btn btn-secondary" name="operator"
-                value="-" title="Kurang"><i class="fas fa-minus"></i></button>
-                <button type="submit" class="btn btn-success" name="operator"
-                value="*" title="Kali"><i class="fas fa-xmark"></i></button>
-                <button type="submit" class="btn btn-info" name="operator"
-                value="/" title="Bagi"><i class="fas fa-divide"></i></button>
-                |
-                <button type="reset" name="reset" class="btn btn-warning" value="reset" title="Clear Entry">CE</button>
-             </div>
-        </form>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-         <div class=" p-2 border rounded bg-light ">
-            <h4 class="text-center">
-              <?php
-              if (isset($_POST['operator'])) {
-                 $angka1 = $_POST['angka1'];
-                 $angka2 = $_POST['angka2'];
-                 $operator = $_POST['operator'];
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-                 if (!is_numeric($angka1) || !is_numeric($angka2)) {
-                   echo  "<script>alert('Input harus berupa angka')</script>";
-                 }elseif($operator == '/' && $angka2 == 0){
-                    echo  "<script>alert('Tidak dapat membagi dengan Nol')</script>";
-                 }else{
-                    switch ($operator) {
-                        case '+':
-                            $hasil = $angka1 + $angka2;
-                            break;
-                        case '-':
-                            $hasil = $angka1 - $angka2;
-                            break;
-                         case '*':
-                            $hasil = $angka1 * $angka2;
-                             break;
-                         case '/':
-                            $hasil = $angka1 / $angka2;
-                             break; 
-
-                        default:
-                            echo "operator tidak valid";
-                            break;
-                    }
-                    echo "$angka1 $operator $angka2 = $hasil";
-                 }
-              }else{
-                echo "Hasil : ";
-              }
-               ?>
-            </h4>
-
-
-            <div class="row">
-                <div class="col-6">
-                    <?php if(!empty($hasil)) : ?>
-                        <form method="POST">                         
-                             <input type="hidden" name="hasil" value="<?php echo $hasil ?>">
-                        <button type="submit" class="btn btn-primary" title="Memory Entry">ME</button>
-                    </form>
-                    <?php endif; ?>
-                    </div>
-                <div class="col-6">
-                    <?php if(isset($hasil) && $hasil !== null) : ?>
-                        <form method="POST">
-                <button type="submit" name="resethasil" class="btn btn-danger" title="Memory Entry">MC</button>
-                    </form>
-                    <?php endif; ?>
-
-            </div>
-        </div>
+    <title>web, andre!</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">HOME</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="form.php">Form</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
     </div>
-   </div>
- </div>
+  </div>
+</nav>
 
- <p class="text-center">&copy; UKK RPL 2025 | Kelas</p>
+    <h1>Hello, world!</h1>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js">
-</body>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
 </html>
